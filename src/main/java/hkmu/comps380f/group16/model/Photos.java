fbfc97e8fbb367fe137a349ff7c7d347127f294b
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-07 07:59:09 +0800

    Updated Photos.java

--- src/main/java/hkmu/comps380f/group16/model/Photos.java
+++ src/main/java/hkmu/comps380f/group16/model/Photos.java
@@ -2,23 +2,43 @@ package hkmu.comps380f.group16.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 //@Entity
 //@Table(name="photo")
 public class Photos {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.UUID)
-//    @Column(name = "photo_id")
-//    private long photoID;
-//
-//    @Column(name = "photo_title")
-//    private String photoTitle;
-//
-//    @Column(name = "photo_data")
-//    private byte[] photoData;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "photo_id")
+    private long photoID;
 
 
-    // foeignt key user id
+
+    @Column(name = "photo_title")
+    private String photoTitle;
+
+    @Column(name = "photo_data")
+    private byte[] photoData;
+
+    @Column(name = "photo_file_type")
+    private String photoFileType;
+
+    @Column(name = "photo_description")
+    private String photoDescription;
+
+    @Column(name = "photo_update_datetime")
+    private Date photoUpdateDatetime;
+
+    // foreign key
+    @Column(insertable = false, updatable = false)
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "username")
+    private PhotoBlogUsers photoBlogUsers;
+
+
 
 
 
