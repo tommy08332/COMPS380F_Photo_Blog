f8337773df55678666522be0568e028297fdf119
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-07 01:40:16 +0800

    Updated UserRole.java

--- src/main/java/hkmu/comps380f/group16/model/UserRole.java
+++ src/main/java/hkmu/comps380f/group16/model/UserRole.java
@@ -2,19 +2,59 @@ package hkmu.comps380f.group16.model;
 
 import jakarta.persistence.*;
 
-//@Entity
-//@Table(name="userRole")
+@Entity
+@Table(name="photoblog_user_role")
 public class UserRole {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.UUID)
-//    @Column(name = "user_role_id")
-//    private long userRoleId;
-//
-//    @Column(name = "user_role")
-//    private long userRole;
-//
-//    // for foreign key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_role_id")
+    private long userRoleId;
 
 
+    @Column(name = "user_role")
+    private String username;
+    private long userRole;
+
+
+    // for foreign key
+    @ManyToOne
+    @JoinColumn(name = "username")
+    private PhotoBlogUsers photoBlogUsersusers;
+
+
+
+
+    public long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(long userRole) {
+        this.userRole = userRole;
+    }
+
+    public PhotoBlogUsers getPhotoBlogUsersusers() {
+        return photoBlogUsersusers;
+    }
+
+    public void setPhotoBlogUsersusers(PhotoBlogUsers photoBlogUsersusers) {
+        this.photoBlogUsersusers = photoBlogUsersusers;
+    }
 }
