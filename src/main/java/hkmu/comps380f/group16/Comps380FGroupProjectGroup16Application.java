843910aad78e020a10a83faf139ae0246c8dfb7e
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-06 22:02:29 +0800

    v1.0
    Added Gradle dependencies

---
+++ src/main/java/hkmu/comps380f/group16/Comps380FGroupProjectGroup16Application.java
@@ -0,0 +1,13 @@
+package hkmu.comps380f.group16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comps380FGroupProjectGroup16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comps380FGroupProjectGroup16Application.class, args);
+    }
+
+}
---
+++ src/test/java/hkmu/comps380f/group16/Comps380FGroupProjectGroup16ApplicationTests.java
@@ -0,0 +1,13 @@
+package hkmu.comps380f.group16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comps380FGroupProjectGroup16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
