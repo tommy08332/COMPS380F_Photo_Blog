7aeb09d380f7da9b7e9601957a35311c193350ef
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-07 16:39:30 +0800

    Updated data.sql

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,1 +1,5 @@
-insert into photoblog_users (username, user_password) VALUES ( 'Test12324', 'P@ssw0rd' );
+insert into photoblog_users (username, user_password) values ('testuser', '{noop}testuserpw');
+
+insert into photoblog_user_role (user_role_id, username, user_role) values (default, 'testuser', 'ROLE_ADMIN');
+
+insert into photoblog_user_role (user_role_id, username, user_role) values (default, 'testuser', 'ROLE_USER');
