35d0ea86a6474cde0fbabae7a360e9479fecaa2c
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-07 09:43:35 +0800

    Created PhotoBlogUsersService.java

---
+++ src/main/java/hkmu/comps380f/group16/dao/PhotoBlogUsersService.java
@@ -0,0 +1,17 @@
+package hkmu.comps380f.group16.dao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhotoBlogUsersService {
+
+    private PhotoBlogUsersRepository usersRepository;
+
+    private UserRoleRepository userRoleRepository;
+
+
+
+
+
+
+}
