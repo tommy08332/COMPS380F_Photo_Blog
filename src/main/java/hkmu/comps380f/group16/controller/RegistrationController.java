0e0d7aea75320fbded24db4ccc6028a42376dcb6
A-Unknowner <116628628+A-Unknowner@users.noreply.github.com>
2023-04-07 21:41:14 +0800

    update

--- src/main/java/hkmu/comps380f/group16/controller/RegistrationController.java
+++ src/main/java/hkmu/comps380f/group16/controller/RegistrationController.java
@@ -1,5 +1,6 @@
 package hkmu.comps380f.group16.controller;
 
+import hkmu.comps380f.group16.applicationForm;
 import hkmu.comps380f.group16.dao.PhotoBlogUsersService;
 import hkmu.comps380f.group16.model.PhotoBlogUsers;
 import jakarta.annotation.Resource;
@@ -51,41 +52,6 @@ public class RegistrationController {
         return "redirect:/login";
     }
 
-    public static class applicationForm{
 
-        private String username;
-        private String password;
-        private String[] userRole;
-
-        public String getUsername() {
-
-            return username;
-        }
-
-        public void setUsername(String username) {
-
-            this.username = username;
-        }
-
-        public String getPassword() {
-
-            return password;
-        }
-
-        public void setPassword(String password) {
-
-            this.password = password;
-        }
-
-        public String[] getUserRole() {
-
-            return userRole;
-        }
-
-        public void setUserRole(String[] userRole) {
-
-            this.userRole = userRole;
-        }
-    }
 
 }
