38208a1af99b26b9ac2aa5d9da3d29a3bd0a5a5b
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-07 08:06:34 +0800

    Updated Photos.java

--- src/main/java/hkmu/comps380f/group16/model/Photos.java
+++ src/main/java/hkmu/comps380f/group16/model/Photos.java
@@ -4,16 +4,14 @@ import jakarta.persistence.*;
 
 import java.util.Date;
 
-//@Entity
-//@Table(name="photo")
+@Entity
+@Table(name="photo")
 public class Photos {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "photo_id")
     private long photoID;
-
-
 
     @Column(name = "photo_title")
     private String photoTitle;
@@ -27,8 +25,8 @@ public class Photos {
     @Column(name = "photo_description")
     private String photoDescription;
 
-    @Column(name = "photo_update_datetime")
-    private Date photoUpdateDatetime;
+    @Column(name = "photo_uploaded_datetime")
+    private Date photoUploadedDatetime;
 
     // foreign key
     @Column(insertable = false, updatable = false)
@@ -39,7 +37,83 @@ public class Photos {
     private PhotoBlogUsers photoBlogUsers;
 
 
+    public Photos(){};
 
+    public Photos(String photoTitle,
+                  byte[] photoData,
+                  String photoFileType,
+                  String photoDescription,
+                  Date photoUploadedDatetime){
 
+        this.photoTitle = photoTitle;
+        this.photoData = photoData;
+        this.photoFileType = photoFileType;
+        this.photoDescription = photoDescription;
+        this.photoUploadedDatetime = photoUploadedDatetime;
 
+    }
+
+    public long getPhotoID() {
+        return photoID;
+    }
+
+    public void setPhotoID(long photoID) {
+        this.photoID = photoID;
+    }
+
+    public String getPhotoTitle() {
+        return photoTitle;
+    }
+
+    public void setPhotoTitle(String photoTitle) {
+        this.photoTitle = photoTitle;
+    }
+
+    public byte[] getPhotoData() {
+        return photoData;
+    }
+
+    public void setPhotoData(byte[] photoData) {
+        this.photoData = photoData;
+    }
+
+    public String getPhotoFileType() {
+        return photoFileType;
+    }
+
+    public void setPhotoFileType(String photoFileType) {
+        this.photoFileType = photoFileType;
+    }
+
+    public String getPhotoDescription() {
+        return photoDescription;
+    }
+
+    public void setPhotoDescription(String photoDescription) {
+        this.photoDescription = photoDescription;
+    }
+
+    public Date getPhotoUploadedDatetime() {
+        return photoUploadedDatetime;
+    }
+
+    public void setPhotoUploadedDatetime(Date photoUploadedDatetime) {
+        this.photoUploadedDatetime = photoUploadedDatetime;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public PhotoBlogUsers getPhotoBlogUsers() {
+        return photoBlogUsers;
+    }
+
+    public void setPhotoBlogUsers(PhotoBlogUsers photoBlogUsers) {
+        this.photoBlogUsers = photoBlogUsers;
+    }
 }
