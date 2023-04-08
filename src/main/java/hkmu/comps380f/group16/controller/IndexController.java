7ce34aad0f555d86bf0cf58eb2b75d8aa914ec0f
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-09 05:21:19 +0800

    Update IndexController.java

--- src/main/java/hkmu/comps380f/group16/controller/IndexController.java
+++ src/main/java/hkmu/comps380f/group16/controller/IndexController.java
@@ -1,22 +1,49 @@
 package hkmu.comps380f.group16.controller;
 
-import hkmu.comps380f.group16.dao.PhotoBlogUsersService;
-import hkmu.comps380f.group16.model.PhotoBlogUsers;
+import hkmu.comps380f.group16.dao.PhotosService;
+import hkmu.comps380f.group16.exception.PhotoNotFound;
+import hkmu.comps380f.group16.model.Photos;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Resource
-    PhotoBlogUsersService usersService;
-
+    private PhotosService photosService;
+    
     @GetMapping("/")
-    public String index(){ return "index"; }
+    public String index(ModelMap model) throws PhotoNotFound, UnsupportedEncodingException {
 
+        List<Photos> photos = photosService.findAllPhotos();
 
+        // may have some problem here
+        ArrayList<String> filesArr = new ArrayList<String>();
+        
+        for (Photos photo : photos){
+
+            byte [] imageByteArr = photo.getPhotoData();
+            byte [] p = Base64.getEncoder().encode(imageByteArr);
+            String photoImg = new String(p, "UTF-8");
+
+            filesArr.add(photoImg);
+            
+        }
+        
+        model.addAttribute("photos", photos);
+
+        model.addAttribute("fileContent", filesArr);
+
+        return "index";
+
+    }
 
     @GetMapping("/login")
     public String login(){
@@ -27,6 +54,5 @@ public class IndexController {
     public String registration(){
         return "registration";
     }
-
-
+    
 }
