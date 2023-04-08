4d1cfe015484eae5bdac57a3ee10aad07852381d
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-08 12:43:28 +0800

    Updated PhotosRepository.java

--- src/main/java/hkmu/comps380f/group16/dao/PhotosRepository.java
+++ src/main/java/hkmu/comps380f/group16/dao/PhotosRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface PhotosRepository extends JpaRepository<Photos, UUID> {
+public interface PhotosRepository extends JpaRepository<Photos, Integer> {
 }
