cde8d942834e903ea346ed89d21cc546c69c004e
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-09 17:37:31 +0800

    Update UserController.java

--- src/main/java/hkmu/comps380f/group16/controller/UserController.java
+++ src/main/java/hkmu/comps380f/group16/controller/UserController.java
@@ -2,13 +2,30 @@ package hkmu.comps380f.group16.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
+//@RequestMapping("/user")
 public class UserController {
 
+    // for testing
     @GetMapping("/profile")
-    public String profile(){ return "profile"; }
+    // for testing
+
+    // for testing
+//    @GetMapping("/show/{username:.+}")
+//    // for testing
+//
+//    public String profile(@PathVariable("username") String username){
+
+    public String profile(){
+
+
+        System.out.println("Running profile");
+        System.out.println();
+        return "profile";
+
+
+    }
+
 
 }
