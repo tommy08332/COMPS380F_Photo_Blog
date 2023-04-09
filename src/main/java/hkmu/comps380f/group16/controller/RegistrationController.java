ce4d414d855e42dc8cd057c589e7a353cfa321ee
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-04-09 16:31:45 +0800

    Put the applicationForm class to RegistrationController.java

--- src/main/java/hkmu/comps380f/group16/controller/RegistrationController.java
+++ src/main/java/hkmu/comps380f/group16/controller/RegistrationController.java
@@ -1,14 +1,11 @@
 package hkmu.comps380f.group16.controller;
 
-import hkmu.comps380f.group16.applicationForm;
 import hkmu.comps380f.group16.dao.PhotoBlogUsersService;
 import hkmu.comps380f.group16.exception.UserNotFound;
-import hkmu.comps380f.group16.model.PhotoBlogUsers;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,22 +26,22 @@ public class RegistrationController {
     public ModelAndView create(){
 
         return new ModelAndView("registration",
-                               "createPhotoUser",
-                                new applicationForm());
+                "createPhotoUser",
+                new applicationForm());
     }
 
     @PostMapping("/create")
     public View create(applicationForm appForm) throws IOException, UserNotFound {
 
         usersService.createUserAccount(appForm.getUsername(),
-                                       appForm.getPassword(),
-                                       appForm.getUserRole());
+                appForm.getPassword(),
+                appForm.getUserRole());
 
         return new RedirectView("/PhotoBlog/login");
 
     }
 
-//    For test
+    //    For test
     @GetMapping("/result")
     public String result(ModelMap model){
 
@@ -54,5 +51,41 @@ public class RegistrationController {
     }
 
 
+    public class applicationForm{
+
+        private String username;
+        private String password;
+        private String[] userRole;
+
+        public String getUsername() {
+
+            return username;
+        }
+
+        public void setUsername(String username) {
+
+            this.username = username;
+        }
+
+        public String getPassword() {
+
+            return password;
+        }
+
+        public void setPassword(String password) {
+
+            this.password = password;
+        }
+
+        public String[] getUserRole() {
+
+            return userRole;
+        }
+
+        public void setUserRole(String[] userRole) {
+
+            this.userRole = userRole;
+        }
+    }
 
 }
