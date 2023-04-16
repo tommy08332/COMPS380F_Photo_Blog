81d07cb123a3ace2e41d8d41fbc9dffb84d9a560
A-Unknowner <116628628+A-Unknowner@users.noreply.github.com>
2023-04-17 00:13:16 +0800

    update

--- src/main/java/hkmu/comps380f/group16/dao/CommentsRepository.java
+++ src/main/java/hkmu/comps380f/group16/dao/CommentsRepository.java
@@ -3,6 +3,8 @@ package hkmu.comps380f.group16.dao;
 import hkmu.comps380f.group16.model.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
   List<Comments> findByPhotoId(int photoId);
 }
--- src/main/java/hkmu/comps380f/group16/dao/PhotosRepository.java
+++ src/main/java/hkmu/comps380f/group16/dao/PhotosRepository.java
@@ -3,6 +3,8 @@ package hkmu.comps380f.group16.dao;
 import hkmu.comps380f.group16.model.Photos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotosRepository extends JpaRepository<Photos, Integer> {
   
     List<Photos> findByUploadUsername(String username);
