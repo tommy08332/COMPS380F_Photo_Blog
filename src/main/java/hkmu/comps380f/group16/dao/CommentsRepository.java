2682b554930a1b67df28d0d11d635c24f6a0bf41
A-Unknowner <116628628+A-Unknowner@users.noreply.github.com>
2023-04-16 22:04:55 +0800

    Update CommentsRepository.java

--- src/main/java/hkmu/comps380f/group16/dao/CommentsRepository.java
+++ src/main/java/hkmu/comps380f/group16/dao/CommentsRepository.java
@@ -4,4 +4,5 @@ import hkmu.comps380f.group16.model.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
+  List<Comments> findByPhotoId(int photoId);
 }
