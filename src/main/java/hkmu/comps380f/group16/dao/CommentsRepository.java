82a40dfe7c9c0372ee60d4b8ab5b2d0a5e822c8b
A-Unknowner <116628628+A-Unknowner@users.noreply.github.com>
2023-04-17 15:04:51 +0800

    Update CommentsRepository.java

--- src/main/java/hkmu/comps380f/group16/dao/CommentsRepository.java
+++ src/main/java/hkmu/comps380f/group16/dao/CommentsRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
   List<Comments> findByPhotoId(int photoId);
+  List<Comments> findByUsername(String username);
 }
